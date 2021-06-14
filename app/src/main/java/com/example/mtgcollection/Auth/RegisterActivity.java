01b659fc6aee09bbd831a9a389e52bf18015e087
Peter van Rijn <s1105390@student.hsleiden.nl>
2021-06-14 12:44:41 +0200

    register layout done

---
+++ app/src/main/java/com/example/mtgcollection/Auth/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.mtgcollection.Auth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mtgcollection.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
