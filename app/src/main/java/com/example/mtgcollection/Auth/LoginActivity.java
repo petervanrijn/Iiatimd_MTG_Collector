cdbed90bfc48ebce28ca10f960d59da2075a0111
Peter van Rijn <s1105390@student.hsleiden.nl>
2021-06-14 14:05:10 +0200

    added icons on login and register

--- app/src/main/java/com/example/mtgcollection/Auth/LoginActivity.java
+++ app/src/main/java/com/example/mtgcollection/Auth/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.example.mtgcollection.Auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mtgcollection.R;
 
-public class LoginActivity extends AppCompatActivity {
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText username;
+    EditText password;
+    Button loginBtn;
+    Button registerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        Intent toRegister = new Intent(this, RegisterActivity.class);
+        startActivity(toRegister);
+
     }
 }
--- app/src/main/java/com/example/mtgcollection/Auth/RegisterActivity.java
+++ app/src/main/java/com/example/mtgcollection/Auth/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.example.mtgcollection.Auth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mtgcollection.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    Button register;
+    ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        backBtn =  findViewById(R.id.backButton);
+        register = findViewById(R.id.register);
+        backBtn.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == register){
+            finishActivity(200);
+        }else{
+            finish();
+        }
     }
 }
