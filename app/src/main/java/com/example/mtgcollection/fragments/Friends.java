f26b54eb0a60a284d9f12082b67108df358d4436
Burak-Kivrak <s1117029@student.hsleiden.nl>
2021-07-03 15:51:20 +0200

    merge conflict

--- app/src/main/java/com/example/mtgcollection/fragments/Friends.java
+++ app/src/main/java/com/example/mtgcollection/fragments/Friends.java
@@ -1,9 +1,7 @@
 package com.example.mtgcollection.fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +10,11 @@ import com.example.mtgcollection.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TestDestinationTrade#newInstance} factory method to
+ * Use the {@link Friends#newInstance} factory method to
  * create an instance of this fragment.
+ *
  */
-public class TestDestinationTrade extends Fragment {
+public class Friends extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,26 +25,26 @@ public class TestDestinationTrade extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TestDestinationTrade() {
-        // Required empty public constructor
-    }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TestDestinationTrade.
+     * @return A new instance of fragment destination_friends.
      */
     // TODO: Rename and change types and number of parameters
-    public static TestDestinationTrade newInstance(String param1, String param2) {
-        TestDestinationTrade fragment = new TestDestinationTrade();
+    public static Friends newInstance(String param1, String param2) {
+        Friends fragment = new Friends();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public Friends() {
+        // Required empty public constructor
     }
 
     @Override
@@ -61,6 +60,6 @@ public class TestDestinationTrade extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test_destination_trade, container, false);
+        return inflater.inflate(R.layout.fragment_destination_friends, container, false);
     }
 }
